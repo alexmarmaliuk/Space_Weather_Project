7c5e208727a61e8760156c78112761287e89e056
Oleksandr Marmaliuk <oleksandrmarmaliuk@gmail.com>
2024-03-29 23:43:51 +0200

    streamlit attempt

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# coursework
+# COURSEWORK
+
+Interactive app for studying space weather based on geomagnetic field variations and magnetic storms in the Odessa magnetic anomaly zone.
--- sup.py
+++ sup.py
@@ -1,5 +1,11 @@
 from datetime import datetime, timedelta
 import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import fftpack
+from functools import partial
+from scipy.signal import butter, lfilter
 
 def generate_date_array(n, start_date='2024-01-01'):
     """
@@ -25,3 +31,38 @@ def generate_date_array(n, start_date='2024-01-01'):
         start_date += timedelta(days=1)
     
     return np.array(date_array)
+
+def get_moving_avg(y, window_size):
+    # window_size = 50
+    return pd.Series(y).rolling(window=window_size).mean()
+
+
+def block_mean(original_array, k):
+    original_array = np.array(original_array)
+    num_blocks = len(original_array) // k
+    blocks = original_array[:num_blocks*k].reshape(num_blocks, k)
+    mean_array = np.mean(blocks, axis=1)
+    return mean_array
+
+def plot(x, y, label='', fig_size=15):
+    fig = plt.figure(figsize=(fig_size, fig_size/2))
+    plt.plot(x, y, label=label)
+    plt.show()
+    
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def bandpass(df, fs, lowcut=500.0, highcut=1250.0):
+    # lowcut = 500.0
+    # highcut = 1250.0
+    return butter_bandpass_filter(df.y, lowcut, highcut, fs = 2*fs)
