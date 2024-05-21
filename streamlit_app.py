ea5b45f694a34777b9e62c563328c4b4ab51eec6
Oleksandr Marmaliuk <oleksandrmarmaliuk@gmail.com>
2024-05-21 16:07:53 +0300

    aaaaaaaaaaahhhhhhhhhhhhhhhh

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,4 @@ from pages.additional.sup import *
 # Page title
 st.set_page_config(page_title='Interactive Geomagnetic Field Variations Data Explorer', page_icon='📊')
 st.title('📊 Demo')
-
-st.sidebar.success("Select a demo.")
+# st.sidebar.success("Select a demo.")
